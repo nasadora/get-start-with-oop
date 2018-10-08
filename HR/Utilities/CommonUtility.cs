69503574e078169c8b4f7fd943bbdca182a18b18
Adora <nasadora_mohddan@hotmail.com>
2018-10-09 07:56:42 +0800

    Add files via upload

---
+++ HR/Utilities/CommonUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetStartWithOOP.HR.Utilites
+{
+    static class CommonUtility
+    {
+        public static string GetFullname(HR.Employee e, bool FnThenLN = true)
+        {
+            var order = FnThenLN ? "{0} {1}" : "{1} {0}";
+            return String.Format(order, e.Firstname, e.Lastname);
+        }
+    }
+}
