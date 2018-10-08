37695f163048074531c4c049fee2853e790deb41
Adora <nasadora_mohddan@hotmail.com>
2018-10-08 21:50:16 +0800

    Update Meeting.cs

--- Events/Meeting.cs
+++ Events/Meeting.cs
@@ -1,1 +1,28 @@
-#empty
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetStartWithOOP.Events
+{
+    class Meeting<THoster, TAttendee, TAssist>
+        where THoster : class
+        where TAttendee : HR.Employee
+        where TAssist : TAttendee
+    {
+        public THoster Hoster;
+        public List<TAttendee> Attendees;
+        public TAssist Assistance;
+
+        public void StrartMeeting()
+        {
+            
+        }
+
+        public void AddAttentee(TAttendee attendee)
+        {
+        }
+    }
+}
+
