102f541c0593612237575f55bd2ad24c3cbbe82a
Adora <nasadora_mohddan@hotmail.com>
2018-10-08 21:42:06 +0800

    Add files via upload

---
+++ Meeting.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetStartWithOOP.Events
+{
+    class Meeting<THoster, TAttendee, TAssist>
+        where THoster : class
+        where TAttendee : HR.Employee
+        where TAssist : TAttendee
+    {
+        public THoster Hoster;
+        public List<TAttendee> Attendees;
+        public TAssist Assistance;
+
+        public void StrartMeeting()
+        {
+            
+        }
+
+        public void AddAttentee(TAttendee attendee)
+        {
+        }
+    }
+}
