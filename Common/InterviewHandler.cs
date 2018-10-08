185f05c0fef85516f74154ab52367ca5f0dedba9
Adora <nasadora_mohddan@hotmail.com>
2018-10-08 21:48:27 +0800

    Add files via upload

---
+++ Common/InterviewHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetStartWithOOP.Common
+{
+    public delegate bool InterviewHandler (string fullname, byte age);
+}
