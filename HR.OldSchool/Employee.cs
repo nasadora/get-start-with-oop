90873a97e14a38837c8437646a71018b2d0ffcc1
Adora <nasadora_mohddan@hotmail.com>
2018-10-09 07:59:06 +0800

    Add files via upload

---
+++ HR.OldSchool/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetStartWithOOP.Common;
+
+namespace GetStartWithOOP.HR.OldSchool
+{
+    class Employee
+    {
+        public int ID { get; private set; }
+        public string Firstname { get; set; }
+        public string Lasrname { get; set; }
+        public Gender Gender { get; private set; }
+
+        public void Employ()
+        {
+        }
+    }
+}
---
+++ HR.OldSchool/PartTimeEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetStartWithOOP.Common;
+
+namespace GetStartWithOOP.HR.OldSchool
+{
+    class PartTimeEmployee
+    {
+        public int ID { get; private set; }
+        public string Firstname { get; set; }
+        public string Lasrname { get; set; }
+        public Gender Sex { get; private set; }
+
+        public double HourlyRate { get; set; }
+        public double WorkingHours { get; set; }
+        public double Salary { get { return this.HourlyRate * this.WorkingHours; } }
+
+        public void Employ()
+        {
+        }
+    }
+}
