eb34f241182af4d2603b574965854d0ada66104d
Adora <nasadora_mohddan@hotmail.com>
2018-10-09 08:00:40 +0800

    Add files via upload

---
+++ IT/Vendor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetStartWithOOP.IT
+{
+    class Vendor : Common.IContractable
+    {
+        public int VendorID { get; private set; }
+        public string Name { get; set; }
+        internal string VendorContractCode { get; set; }
+
+        string Common.IContractable.GetContractNumber()
+        {
+            return this.VendorContractCode;
+        }
+
+        bool Common.IContractable.IsValid()
+        {
+            return (this.VendorContractCode != String.Empty);
+        }
+    }
+}
